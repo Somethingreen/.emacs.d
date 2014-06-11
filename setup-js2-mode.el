900d1262a549758ad7c15ed661668fa6681a4e7d
Sergey Galkin <s.galkin.pub@gmail.com>
2014-06-11 09:55:30 +0300

    JS mode tweaks * enable electric-pair * switch to auto-indent

--- setup-js2-mode.el
+++ setup-js2-mode.el
@@ -2,11 +2,13 @@
 (add-hook 'js-mode-hook 'js2-minor-mode)
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 (add-hook 'js2-mode-hook 'auto-indent-mode)
+(add-hook 'js2-mode-hook 'electric-pair-mode)
 
-(setq-default js2-auto-indent-p t)
+;;(setq-default js2-auto-indent-p t)
 (custom-set-variables  
  '(js2-basic-offset 4)
- '(js2-bounce-indent-p t))
+;; '(js2-bounce-indent-p t)
+)
 
 ;;(require 'paredit)
 ;;(define-key js2-mode-map "{" 'paredit-open-curly)
